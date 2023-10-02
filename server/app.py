2c39d8d1ed4574f71b82364ba1a28dbb52b1fe1d
Absalom <mrclabsalom@gmail.com>
2023-10-02 07:24:31 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,26 +1,57 @@
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_migrate import Migrate
-
-from models import db, Message
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import Message, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-CORS(app)
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# ... (previous code)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# Routes
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    message_list = [message.to_dict() for message in messages]
+    return jsonify(message_list)
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    body = data.get('body')
+    username = data.get('username')
+
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+
+    data = request.get_json()
+    body = data.get('body')
+
+    message.body = body
+    db.session.commit()
+
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return jsonify({'message': 'Message deleted'}), 200
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
